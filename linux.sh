d0fa6b1c558c6c55627ea203b9754caa4ed5c947
Marcokie <marco.vanstaden@gmail.com>
2021-04-29 19:23:05 +0800

    emacs and python

---
+++ linux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Update packages
+yum update -q -y
+
+# Emacs setup
+yum install emacs -q -y
+# Python with web3 setup
+yum install gcc -q -y
+yum install python3-devel -q -y
+python3 -m pip install web3 -q -y
